00227d63e08b5267f65bacb75c80c5d39457d734
Emily Berkeley <eberkeley@gmail.com>
2015-03-29 12:02:33 -0600

    Add jquery filter

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -1,12 +1,17 @@
 class UrlsController < ApplicationController
+  require "uri"
+
   def index
     @urls = Url.order(click_count: :desc)
   end
 
   def create
-    @url = Url.find_or_create_by(url: params[:url])
-    flash[:message] = "Your new url is: #{@url.short_url}"
-
+    if url_valid?
+      @url = Url.find_or_create_by(url: params[:url])
+      flash[:message] = "Your short url is: #{@url.short_url}"
+    else
+      flash[:error] = "Please enter a valid url(including http:// or https://)"
+    end
     redirect_to root_path
   end
 
@@ -15,5 +20,12 @@ class UrlsController < ApplicationController
     @url.increment!(:click_count)
 
     redirect_to @url.url
+  end
+
+  private
+
+  def url_valid?
+    uri = URI.parse(params[:url])
+    uri.kind_of?(URI::HTTP)
   end
 end
